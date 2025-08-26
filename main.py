3d7f0be6c419cac9b16fea0299907cf1ae83a4cc
Andrew Noble <aknoble.andrew@gmail.com>
2025-08-25 22:11:18 -0400

    simple little shoulders, hips, and wrists inference!

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import cv2
+from ultralytics import YOLO
+
+# pose model
+pmodel = YOLO("yolo11n-pose.pt")
 
 # 0 = default webcam (use 1, 2, ... for other cameras)
 cap = cv2.VideoCapture(0)
@@ -7,6 +11,22 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+
+    results = pmodel(frame, verbose=False)[0]
+    # print(results.keypoints.xy[0])
+
+    for index, (x, y) in enumerate(results.keypoints.xy[0]):
+        xc = int(x.item())
+        yc = int(y.item())
+
+        # cv2.circle(frame, (xc,yc), 5, (0, 0, 255), -1)
+
+        if index in [5,6]: #shoulders
+            cv2.circle(frame, (xc,yc), 10, (0, 0, 255), -1)
+        elif index in [11, 12]:#hips
+            cv2.circle(frame, (xc,yc), 10, (0, 255, 0), -1)
+        elif index in [9, 10]: #wrists
+            cv2.circle(frame, (xc,yc), 10, (255, 0, 0), -1)
 
     cv2.imshow("Webcam", frame)
 
