54e7fb7fa2ecb69a521de6a7d1b83cad78a9c7df
Andrew Noble <aknoble.andrew@gmail.com>
2025-09-01 18:12:19 -0400

    fix crashing bug, add connector lines

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import cv2
 from ultralytics import YOLO
 
-# pose model
 pmodel = YOLO("yolo11n-pose.pt")
-
-# 0 = default webcam (use 1, 2, ... for other cameras)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
@@ -13,24 +10,27 @@ while True:
         break
 
     results = pmodel(frame, verbose=False)[0]
-    # print(results.keypoints.xy[0])
+    kps = results.keypoints.xy  # shape (17,2)
 
-    for index, (x, y) in enumerate(results.keypoints.xy[0]):
-        xc = int(x.item())
-        yc = int(y.item())
+    def pt(i):
+        return tuple(map(int, kps[i].tolist()))
 
-        # cv2.circle(frame, (xc,yc), 5, (0, 0, 255), -1)
+    if len(kps) > 0:
+        kps = kps[0]
+        # torso: shoulders → hips
+        cv2.line(frame, pt(5), pt(6), (0,0,255), 2)   # shoulders
+        cv2.line(frame, pt(11), pt(12), (0,255,0), 2) # hips
+        cv2.line(frame, pt(5), pt(11), (255,255,0), 2)
+        cv2.line(frame, pt(6), pt(12), (255,255,0), 2)
 
-        if index in [5,6]: #shoulders
-            cv2.circle(frame, (xc,yc), 10, (0, 0, 255), -1)
-        elif index in [11, 12]:#hips
-            cv2.circle(frame, (xc,yc), 10, (0, 255, 0), -1)
-        elif index in [9, 10]: #wrists
-            cv2.circle(frame, (xc,yc), 10, (255, 0, 0), -1)
+        # femurs: hip → knee
+        cv2.line(frame, pt(11), pt(13), (255,0,0), 3) # left femur
+        cv2.line(frame, pt(12), pt(14), (255,0,0), 3) # right femur
+    else:
+        pass
 
     cv2.imshow("Webcam", frame)
 
-    # press q to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
